c52c2749d8f01c37fabd52a0e35aa6cc6958e67a
CarlosGzz <carlosgzz93@gmail.com>
2016-08-04 17:23:10 -0500

    cambio a encuesta

--- CONTROLLER/encuestaStaff.php
+++ CONTROLLER/encuestaStaff.php
@@ -8,12 +8,15 @@
 		$fechaNacimiento = trim($_POST["fechaNacimiento"]);
 		$gaia = trim($_POST["gaia"]);
 		$rolDeseado = trim($_POST["rolDeseado"]);
+		$pulsera = trim($_POST["pulsera"]);
 		$correo = trim($_POST["correo"]);
 		$telefonoCel = trim($_POST["telefonoCel"]);
-		$query = "INSERT INTO staff(nombre, apellidoPaterno, apellidoMaterno, genero, fechaNacimiento, idGaia, rolDeseado, correo, telefonoCel) 
-                  VALUES ('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$genero', '$fechaNacimiento', '$gaia', '$rolDeseado', '$correo', '$telefonoCel')";
+		echo $pulsera;
 
-        if(!empty($_POST["nombre"]) && !empty($_POST["apellidoPaterno"]) && !empty($_POST["apellidoMaterno"]) && !empty($_POST["fechaNacimiento"])&& !empty($_POST["gaia"])&& !empty($_POST["rolDeseado"])&& !empty($_POST["correo"])&& !empty($_POST["telefonoCel"])){
+		$query = "INSERT INTO staff(nombre, apellidoPaterno, apellidoMaterno, genero, fechaNacimiento, idGaia, rolDeseado, pulsera, correo, telefonoCel) 
+                  VALUES ('$nombre', '$apellidoPaterno', '$apellidoMaterno', '$genero', '$fechaNacimiento', '$gaia', '$rolDeseado','$pulsera', '$correo', '$telefonoCel')";
+
+        if(!empty($_POST["nombre"]) && !empty($_POST["apellidoPaterno"]) && !empty($_POST["apellidoMaterno"]) && !empty($_POST["fechaNacimiento"]) && !empty($_POST["gaia"]) && !empty($_POST["rolDeseado"]) && !empty($_POST["pulsera"]) && !empty($_POST["correo"]) && !empty($_POST["telefonoCel"])){
 			if ($db->query($query) === TRUE) {
 				echo "1";
 			} else {
