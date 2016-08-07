599e28d9e0f20ca3fe2ea82caade23077f159f9b
Carlos Gonzalez <carlosgzz93@gmail.com>
2016-08-07 17:20:41 -0500

    Update connect.php

--- MODEL/connect.php
+++ MODEL/connect.php
@@ -1,12 +1,12 @@
 <?php
-	/*$servername = "us-cdbr-iron-east-04.cleardb.net";
+	$servername = "us-cdbr-iron-east-04.cleardb.net";
 	$username = "be53035c41706e";
 	$password = "9467bfd68022bd8";
-	$dbname = "heroku_b09fc9ff7592b1e";*/
-	$servername = "localhost";
+	$dbname = "heroku_b09fc9ff7592b1e";
+	/*$servername = "localhost";
 	$username = "root";
 	$password = "kobyjzt";
-	$dbname = "LXM";
+	$dbname = "LXM";*/
 
 	// Create connection
 	$db = new mysqli($servername, $username, $password, $dbname);
